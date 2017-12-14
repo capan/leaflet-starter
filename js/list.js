da300b2a74f3cfab8d26a263fe6d8faca7ae312d
capan <capanh@gmail.com>
2017-12-14 19:05:02 +0300

    a

--- js/list.js
+++ js/list.js
@@ -1,5 +1,24 @@
 var ls = document.getElementById("listtype");
 
+
+var app = angular.module("coordList", []);
+app.controller("myCtrl", function($scope) {
+  $scope.products = [1];
+  $scope.addItem = function(){
+    $scope.errortext = "";
+    if($scope.addMe){return addMe;}
+    if($scope.products.indexOf($scope.addMe) == -1){
+      $scope.products.push($scope.addMe);
+    } else {
+      $scope.errortext = "Same Point Can't Be Added !";
+    }
+  }
+  $scope.removeItem = function(x){
+    $scope.errortext = "";
+    $scope.products.splice(x,1);
+  }
+});
+
 ls.addEventListener("click", function(e){
   var lo = document.getElementById("listtype").selectedIndex;
   switch (document.getElementById("listtype").selectedIndex) {
@@ -15,20 +34,5 @@ ls.addEventListener("click", function(e){
   }
 });
 
-var app = angular.module("coordList", []);
-app.controller("myCtrl", function($scope) {
-  $scope.products = ["deneme1","deneme2"];
-  $scope.addItem = function(){
-    $scope.errortext = "";
-    if($scope.addMe){return;}
-    if($scope.products.indexOf($scope.addMe) == -1){
-      $scope.products.push($scope.addMe);
-    } else {
-      $scope.errortext = "Same Point Can't Be Added !";
-    }
-  }
-  $scope.removeItem = function(x){
-    $scope.errortext = "";
-    $scope.products.splice(x,1);
-  }
-});
+
+
