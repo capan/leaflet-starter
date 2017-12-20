a6f0b0973a0153bf31b21565a8fd0241cc4194ca
capan <capanh@gmail.com>
2017-12-20 16:46:11 +0300

    list çalışıyır

--- js/list.js
+++ js/list.js
@@ -1,44 +1,54 @@
 var ls = document.getElementById("typeList");
 
-
 var app = angular.module("coordList", []);
 
-app.controller("myCtrl", function($scope) {
-  
+app.controller("myCtrl", function ($scope) {
+
   $scope.products = [];
-  $scope.addItem = function(){
-    
+  $scope.addItem = function () {
+
     $scope.errortext = "";
 
-    if(!$scope.addMe){
+    if (!$scope.addMe) {
       $scope.errortext = "You should add something !";
       return addMe;
     }
-    if($scope.products.indexOf($scope.addMe) == -1){
-      $scope.products.push($scope.addMe);
-      $scope.products.indexOf($scope.addMe) = addCoord;
-          } else {
+    if ($scope.products.indexOf($scope.addMe) == -1) {
+      var ekle = $scope.addMe.split(" ");
+      if (ekle.length == 2) {
+        $scope.products.push($scope.addMe);
+        MarkerNo = $scope.products.indexOf($scope.addMe)
+        L.marker([ekle[0], ekle[1]]).addTo(mymap)
+          .bindPopup('<b>My Marker' + MarkerNo + '</b> <br/>' + ekle[0] + '<br/>' + ekle[1]).openPopup();
+        mymap.setView([ekle[0], ekle[1]], 13);
+
+      } else {
+        $scope.errortext = "You should seperate Lat Lon with an empty space!";
+      }
+    }
+    else {
       $scope.errortext = "Same Point Can't Be Added !";
     }
+
   }
-  $scope.removeItem = function(x){
+  $scope.removeItem = function (x) {
     $scope.errortext = "";
-    $scope.products.splice(x,1);
+    $scope.products.splice(x, 1);
   }
 });
 
 var myscope = angular.element("myCtrl").scope();
 
-ls.addEventListener("click", function(e){
+ls.addEventListener("click", function (e) {
   var lo = document.getElementById("typeList").selectedIndex;
   switch (document.getElementById("typeList").selectedIndex) {
     case 0:
       document.getElementById("h").innerHTML = "Point List";
       break;
-      case 1:
+    case 1:
       document.getElementById("h").innerHTML = "Line List";
       break;
-      case 2:
+    case 2:
       document.getElementById("h").innerHTML = "Polygon List";
       break;
   }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 	var mymap = L.map('map').setView([39, 35], 6);
     var clickedCoord = [];
     var addCoord;
-
+    var MarkerNo = 0;
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
             id: 'mapbox.streets'
